c01884ac66c207352ad89f3b14920c3577b4c3b8
Your Name <you@example.com>
2024-08-17 01:30:12 +0530

    prefinal

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,23 +4,23 @@ const {postJob, getJob,userApplyForJob,userPostedJobs,userDeletePostedJob, getJo
 const {login,signUp, getCookie, getUser, getUsers,  getPopulatedUserByReq,getPopulatedUserByCon, logout} = require('../controllers/usersRegistration.js')
 const router= Router()
 router.route("/login").post(login);
-router.route("/logout").get(ensureAuthenticated,logout);
+router.route("/logout").get(logout);
 router.route("/sign-up").post(signUp);
-router.route("/get-user").get(ensureAuthenticated,getUser);
-router.route("/get-users").get(ensureAuthenticated,getUsers);
-router.route("/get-populated-user-by-req").get(ensureAuthenticated,getPopulatedUserByReq);
-router.route("/get-populated-user-by-con").get(ensureAuthenticated,getPopulatedUserByCon);
+router.route("/get-user").get(getUser);
+router.route("/get-users").get(getUsers);
+router.route("/get-populated-user-by-req").get(getPopulatedUserByReq);
+router.route("/get-populated-user-by-con").get(getPopulatedUserByCon);
 router.route("/getCookie").get(getCookie);
-router.route("/post-jobs").post(ensureAuthenticated,postJob);
+router.route("/post-jobs").post(postJob);
 router.route("/get-jobs").get(getJob);
-router.route("/get-jobs-by-id").get(ensureAuthenticated,getJobById);
-router.route("/accept-job").get(ensureAuthenticated,acceptJob);
-router.route("/reject-job").get(ensureAuthenticated,rejectJob);
-router.route("/join-job").get(ensureAuthenticated,joinJob);
-router.route("/cancel-job").get(ensureAuthenticated,cancelJob);
-router.route("/application").get(ensureAuthenticated,userApplyForJob);
-router.route("/posted-job").get(ensureAuthenticated,userPostedJobs);
-router.route("/delete-job").get(ensureAuthenticated,userDeletePostedJob);
+router.route("/get-jobs-by-id").get(getJobById);
+router.route("/accept-job").get(acceptJob);
+router.route("/reject-job").get(rejectJob);
+router.route("/join-job").get(joinJob);
+router.route("/cancel-job").get(cancelJob);
+router.route("/application").get(userApplyForJob);
+router.route("/posted-job").get(userPostedJobs);
+router.route("/delete-job").get(userDeletePostedJob);
 
 
 module.exports=router
